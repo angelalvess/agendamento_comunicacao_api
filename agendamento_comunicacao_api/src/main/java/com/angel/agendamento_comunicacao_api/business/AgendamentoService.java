95bbc3debe6ffddb1dd8dda83122dbcfb3a2d596
angelalves <angeltrashangel@gmail.com>
2025-03-07 12:40:59 -0300

    criado e implementado metodo para criar agendamento de comunicacao nas classes AgendamentoService e AgendamentoController

--- agendamento_comunicacao_api/src/main/java/com/angel/agendamento_comunicacao_api/business/AgendamentoService.java
+++ agendamento_comunicacao_api/src/main/java/com/angel/agendamento_comunicacao_api/business/AgendamentoService.java
@@ -4,7 +4,9 @@ package com.angel.agendamento_comunicacao_api.business;
 import com.angel.agendamento_comunicacao_api.business.mapper.AgendamentoMapper;
 import com.angel.agendamento_comunicacao_api.controller.dtos.in.AgendamentoRecord;
 import com.angel.agendamento_comunicacao_api.controller.dtos.out.AgendamentoRecordOut;
+import com.angel.agendamento_comunicacao_api.infrastructure.exceptions.NotFoundException;
 import com.angel.agendamento_comunicacao_api.infrastructure.repositories.AgendamentoRepository;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +14,8 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AgendamentoService {
 
-    private AgendamentoRepository agendamentoRepository;
-    private AgendamentoMapper agendamentoMapper;
+    private final AgendamentoRepository agendamentoRepository;
+    private final AgendamentoMapper agendamentoMapper;
 
 
     public AgendamentoRecordOut criarAgendamento (AgendamentoRecord agendamento) {
@@ -21,4 +23,6 @@ public class AgendamentoService {
         return agendamentoMapper.paraOut(agendamentoRepository.
                 save(agendamentoMapper.paraAgendamento(agendamento)));
     }
+
+
 }
--- agendamento_comunicacao_api/src/main/java/com/angel/agendamento_comunicacao_api/controller/AgendamentoController.java
+++ agendamento_comunicacao_api/src/main/java/com/angel/agendamento_comunicacao_api/controller/AgendamentoController.java
@@ -7,21 +7,24 @@ import com.angel.agendamento_comunicacao_api.controller.dtos.out.AgendamentoReco
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/agendamento")
+@RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class AgendamentoController {
 
-    private AgendamentoService agendamentoService;
+    private final AgendamentoService agendamentoService;
 
 
-    @PostMapping
-    ResponseEntity<AgendamentoRecordOut> criarAgendamento (AgendamentoRecord agendamento) {
+    @PostMapping("/agendamento")
+    public ResponseEntity<AgendamentoRecordOut> criarAgendamento (@RequestBody AgendamentoRecord agendamento) {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(agendamentoService.criarAgendamento(agendamento));
 
     }
+
+
+
 }
 

bf569c8b8be98c88b43aad078dde81b7a6158d01
angelalves <angeltrashangel@gmail.com>
2025-03-07 19:16:38 -0300

    criado e implementado metodos para cancelar agendamento nas classes AgendamentoService e AgendamentoController

--- agendamento_comunicacao_api/src/main/java/com/angel/agendamento_comunicacao_api/controller/AgendamentoController.java
+++ agendamento_comunicacao_api/src/main/java/com/angel/agendamento_comunicacao_api/controller/AgendamentoController.java
@@ -5,29 +5,35 @@ import com.angel.agendamento_comunicacao_api.business.AgendamentoService;
 import com.angel.agendamento_comunicacao_api.controller.dtos.in.AgendamentoRecord;
 import com.angel.agendamento_comunicacao_api.controller.dtos.out.AgendamentoRecordOut;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/api")
 @RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class AgendamentoController {
 
 
-    private final AgendamentoService service;
+    private final AgendamentoService agendamentoService;
 
     @PostMapping("/agendamento")
     public ResponseEntity<AgendamentoRecordOut> criarAgendamento (@RequestBody AgendamentoRecord agendamento) {
 
-        return ResponseEntity.ok(service.criarAgendamento(agendamento));
+        return ResponseEntity.ok(agendamentoService.criarAgendamento(agendamento));
 
     }
 
     @GetMapping("/agendamento/{id}")
-    public ResponseEntity<AgendamentoRecordOut> buscaAgendamento (@PathVariable(name = "id") Long id) {
+    public ResponseEntity<AgendamentoRecordOut> buscaAgendamento (@PathVariable("id") Long id) {
 
-        return ResponseEntity.ok(service.buscaAgendamento(id));
+        return ResponseEntity.ok(agendamentoService.buscaAgendamento(id));
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> cancelaAgendamento (@PathVariable("id") Long id) {
+        agendamentoService.cancelaAgendamento(id);
+        return ResponseEntity.accepted().build();
 
     }
 
